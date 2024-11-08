aee491e1902ffd372cc9c30ac7e7b48df4ddc69d
shekharP1536 <chandrashekharpachlore@gmail.com>
2024-11-08 23:59:18 +0530

    initial_commit

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+let questions = [
+    {
+      numb: 1,
+      question: "Which one is Ai generated ?",
+      answer: "A",
+      options: [
+        "A",
+        "B"
+      ],
+      image: "img/3.png"  // Add the path to your image here
+    },
+    {
+        numb: 2,
+        question: "Which one is Ai generated ?",
+        answer: "A",
+        options: [
+          "A",
+          "B"
+        ],
+        image: "img/6.png"  // Add the path to your image here
+      }
+  ];
+  
---
+++ pjs/custom.js
@@ -0,0 +1,30 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Rate the Meme.",
+    creator : "Shekhar",
+    answer: "A",
+    options: [
+      "&#128514;",
+      "&#128513;",
+      "&#128512;",
+      "&#128078;",
+
+    ],
+    image: "pjs/img/a.jpg"  // Add the path to your image here
+  },
+  {
+      numb: 2,
+      question: "Rate the Meme?",
+      creator : "Shekhar",
+      answer: "A",
+      options: [
+        "&#128514;",
+        "&#128513;",
+        "&#128512;",
+        "&#128078;"
+  
+      ],
+      image: "pjs/img/b.png"  // Add the path to your image here
+    }
+];
